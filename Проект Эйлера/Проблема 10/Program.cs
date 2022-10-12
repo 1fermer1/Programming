8306339d38565542b58a09e7df6ad30671616abb
1fermer1 <114748246+1fermer1@users.noreply.github.com>
2022-10-12 22:29:12 +0300

    Add files via upload

---
+++ Проект Эйлера/Проблема 10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Проблема_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Найдите сумму всех простых чисел меньше двух миллионов.");
+            List<int> nums = new List<int> { 2, 3, 5, 7, 11, 13 };
+            int a = 13;
+            long sum = 41;
+            while (true)
+            {
+                a += 2;
+                if (a > 2000000)
+                {
+                    break;
+                }
+                for (int i = 1; i < nums.Count; i++)
+                {
+                    if (a % nums[i] == 0)
+                    {
+                        break;
+                    }
+                    if (i == nums.Count - 1 && a % nums[i] != 0)
+                    {
+                        if (a < 1500)
+                        {
+                            nums.Add(a);
+                        }
+                        sum += a;
+                    }
+                }
+            }
+            Console.WriteLine("Ответ: " + sum);
+        }
+    }
+}
---
+++ Проект Эйлера/Проблема 12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Проблема_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какое значение имеет первое число треугольника, которое имеет более пятисот делителей?");
+            int a = 0;
+            for (int n = 1; ; n++)
+            {
+                a += n;
+                List<int> del = new List<int>();
+                for (int i = 1; i <=(Math.Sqrt(a)); i++)
+                {
+                    if (a % i == 0)
+                    {
+                        if (a % i == i)
+                        {
+                            del.Add(i);
+                        }
+                        else
+                        {
+                            del.Add(i);
+                            del.Add(a / i);
+                        }
+                    }
+                }
+                if (del.Count > 500)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Ответ: " + a);
+        }
+    }
+}
