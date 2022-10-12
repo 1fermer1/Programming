7cd4101a50448f468d7c8287d05fcd2db731f2f0
1fermer1 <114748246+1fermer1@users.noreply.github.com>
2022-10-12 23:01:47 +0300

    Add files via upload

---
+++ Проблема 41/Pandigital.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Проблема_41
+{
+    class Pandigital
+    {
+        public int InputNumber = 0;
+        private string stringNumber = null;
+        private bool pandigitalNumber = true;
+
+        public bool PandigitalNumber()
+        {
+            List<int> digits = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+            pandigitalNumber = true;
+            stringNumber = InputNumber.ToString();
+            int a = stringNumber.Length;
+            for (int i = 0; i < a && pandigitalNumber; i++)
+            {
+                pandigitalNumber = false;
+                for (int n = 0; n < (a - i); n++)
+                {
+                    if (stringNumber[i].ToString() == digits[n].ToString())
+                    {
+                        pandigitalNumber = true;
+                        digits.RemoveAt(n);
+                        break;
+                    }
+                }
+            }
+            return pandigitalNumber;
+        }
+    }
+}
---
+++ Проблема 43/CheckDivision.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Проблема_43
+{
+    static class CheckDivision
+    {
+        private static int Number = 0;
+        public static bool On3(int a, int b, int c)
+        {
+            Number = (100 * a + 10 * b + c);
+            if ((Number % 3) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool On7(int a, int b, int c)
+        {
+            Number = (100 * a + 10 * b + c);
+            if ((Number % 7) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool On11(int a, int b, int c)
+        {
+            Number = (100 * a + 10 * b + c);
+            if ((Number % 11) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool On13(int a, int b, int c)
+        {
+            Number = (100 * a + 10 * b + c);
+            if ((Number % 13) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool On17(int a, int b, int c)
+        {
+            Number = (100 * a + 10 * b + c);
+            if ((Number % 17) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
