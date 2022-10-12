ff229edf69081f785c0d2efea01043a786965151
1fermer1 <114748246+1fermer1@users.noreply.github.com>
2022-10-12 23:05:45 +0300

    Add files via upload

---
+++ Проект Эйлера/Проблема 41/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Проблема_41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какое наибольшее существующее n-значное панцифровое простое число?");
+            Pandigital Number = new Pandigital();
+            Prime PandigitalNumber = new Prime();
+
+            for (int i = 7654321; i > 1234566; i -= 2)
+            {
+                Number.InputNumber = i;
+                if (Number.PandigitalNumber())
+                {
+                    PandigitalNumber.InputPandigitalNumber = i;
+                    if (PandigitalNumber.PrimePandigitalNumber())
+                    {
+                        Console.WriteLine($"Ответ: {i}");
+                        return;
+                    }
+                }
+            }
+            for (int i = 4321; i > 1233; i -= 2)
+            {
+                Number.InputNumber = i;
+                if (Number.PandigitalNumber())
+                {
+                    PandigitalNumber.InputPandigitalNumber = i;
+                    if (PandigitalNumber.PrimePandigitalNumber())
+                    {
+                        Console.WriteLine($"Ответ: {i}");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
