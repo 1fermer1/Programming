48c90848dec4bbff841d0dfe5547142d50a7f16e
1fermer1 <114748246+1fermer1@users.noreply.github.com>
2022-10-12 22:39:10 +0300

    Add files via upload

---
+++ Проект Эйлера/Проблема 20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Проблема_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Найдите сумму цифр числа 100!");
+			int[] nums = new int[158];
+			nums[157] = 6;
+			for(int i = 4; i < 101; i++)
+            {
+				for(int n = 157; n > 0; n--)
+				{
+					nums[n] *= i;
+				}
+				for(int k = 157; k > 0; k--)
+				{
+					int temp = (nums[k] % 10);
+					int a = (nums[k] - temp);
+					a /= 10;
+					nums[k - 1] += a;
+					nums[k] = temp;
+				}
+			}
+			int sum = 0;
+			foreach (int n in nums)
+		    {
+				sum += n;
+			}
+			Console.WriteLine("Ответ: " + sum);
+		}
+    }
+}
---
+++ Проект Эйлера/Проблема 21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Проблема_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Оцените сумму всех полюбившихся чисел меньше 10000.");
+            int sum = 0, a, b;
+            for (int i = 0; i < 10000; i++)
+            {
+                a = b = 0;
+                for (int n = 1; n <= i/2; n++)
+                {
+                    if ((i % n) == 0)
+                    {
+                        a += n;
+                    }
+                }
+                for (int n = 1; n <= a/2; n++)
+                {
+                    if ((a % n) == 0)
+                    {
+                        b += n;
+                    }
+                }
+                if (b == i && a < i)
+                {
+                    sum += b;
+                    sum += a;
+                }
+            }
+            Console.WriteLine("Ответ: {0}", sum);
+        }
+    }
+}
