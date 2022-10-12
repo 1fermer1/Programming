14da3b60668ebe5da04c562a28f91a6eb621fc61
1fermer1 <114748246+1fermer1@users.noreply.github.com>
2022-10-12 22:32:26 +0300

    Add files via upload

---
+++ Проект Эйлера/Проблема 14/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Проблема_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какое начальное число меньше миллиона дает самую длинную цепочку?");
+            double[] ind = new double[500000];
+            for (int i = 0; i < 500000; i++)
+            {
+                ind[i] = (i + 500000);
+            }
+            for (int i = 0; i < 400; i++)
+            {
+                for (int n = 0; n < 500000; n++)
+                {
+                    if(ind[n] != 16)
+                    {
+                        if (ind[n] % 2 == 0)
+                        {
+                            ind[n] /= 2;
+                        }
+                        else
+                        {
+                            ind[n] = ((3 * ind[n]) + 1);
+                        }
+                    }
+                }
+            }
+            List<double> ind2 = new List<double>();
+            List<double> num = new List<double>();
+            for (int i = 0; i < 500000; i++)
+            {
+                if (ind[i] != 16)
+                {
+                    ind2.Add(ind[i]);
+                    num.Add(i + 500000);
+                }
+            }
+            for (int i = 0; ; i++)
+            {
+                for (int n = 0; n < ind2.Count; n++)
+                {
+                    if (ind2[n] != 16)
+                    {
+                        if (ind2[n] % 2 == 0)
+                        {
+                            ind2[n] /= 2;
+                        }
+                        else
+                        {
+                            ind2[n] = ((3 * ind2[n]) + 1);
+                        }
+                    }
+                    else
+                    {
+                        ind2.RemoveAt(n);
+                        num.RemoveAt(n);
+                    }
+                }
+                if (num.Count == 1)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(num[0]);
+        }
+    }
+}
---
+++ Проект Эйлера/Проблема 15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Проблема_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько существует таких маршрутов через сетку 20 × 20?");
+            long[][] nums = new long[41][];
+            for (int i = 0; i < 41; i++)
+            {
+                nums[i] = new long[i + 1];
+                nums[i][0] = 1;
+                nums[i][i] = 1;
+            }
+            for (int i = 2; i < 41; i++)
+            {
+                for (int n = 1; n < (nums[i].Length - 1); n++)
+                {
+                    nums[i][n] = nums[i - 1][n - 1] + nums[i - 1][n];
+                }
+            }
+            Console.WriteLine("Ответ: " + nums[40][20]);
+        }
+    }
+}
