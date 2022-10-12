f37c618c7027857db5364c2196a5ec22680c16fd
1fermer1 <114748246+1fermer1@users.noreply.github.com>
2022-10-12 22:27:30 +0300

    Add files via upload

---
+++ Проект Эйлера/Проблема 7/Проблема 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Проблема_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какое 10001-е простое число?");
+            List<int> nums = new List<int> { 2, 3, 5, 7, 11, 13 };
+            int a = 13;
+            while (true)
+            {
+                a += 2;
+                if (nums.Count == 10001)
+                {
+                    break;
+                }
+                for (int i = 1; i < nums.Count; i++)
+                {
+                    if (a % nums[i] == 0)
+                    {
+                        break;
+                    }
+                    if (i == nums.Count - 1 && a % nums[i] != 0)
+                    {
+                        nums.Add(a);
+                    }
+                }
+            }
+            Console.WriteLine("Ответ: " + nums[10000]);
+        }
+    }
+}
---
+++ Проект Эйлера/Проблема 9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Проблема_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Существует ровно одна тройка Пифагора, для которой a+b+c=1000. Найдите произведение abc.");
+            int a, b = 0, c = 0;
+            for (a = 1; a < 332; a++)
+            {
+                for (b = a + 1; b < 1000 - (a + b); b++)
+                {
+                    c = (1000 - (a + b));
+                    if (((a*a) + (b*b)) == (c*c))
+                    {
+                        break;
+                    }
+                }
+                if (((a * a) + (b * b)) == (c * c))
+                {
+                    break;
+                }
+            }
+            int r = a * b * c;
+            Console.WriteLine("Ответ: " + r);
+        }
+    }
+}
